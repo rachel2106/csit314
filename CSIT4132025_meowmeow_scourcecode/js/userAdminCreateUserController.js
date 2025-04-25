d98f6f38d2a666ad35dfcfbf15c36bcf66356bbf
rachel2106 <rachel.seet64@gmail.com>
2025-04-25 18:15:38 +0800

    Create userAdminCreateUserController.js

---
+++ CSIT4132025_meowmeow_scourcecode/js/userAdminCreateUserController.js
@@ -0,0 +1,9 @@
+import { userEntity } from "./userEntity.js";
+
+export class adminCreateUserController {
+    async createUserAcc(newUserObj) {
+        const initAdminCUEntity = new userEntity();
+        const message = await initAdminCUEntity.createUser(newUserObj);
+        return message;
+    }
+}
