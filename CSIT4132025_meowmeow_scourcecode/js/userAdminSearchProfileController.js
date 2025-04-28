6358eea9edeeb40feddbeea9f1f22d7612734c18
rachel2106 <rachel.seet64@gmail.com>
2025-04-29 03:19:47 +0800

    Update userAdminSearchProfileController.js

--- CSIT4132025_meowmeow_scourcecode/js/userAdminSearchProfileController.js
+++ CSIT4132025_meowmeow_scourcecode/js/userAdminSearchProfileController.js
@@ -1,10 +1,10 @@
 import { profileEntity } from "./profileEntity.js";
 
 export class userAdminSearchProfileController {
-    async searchProfileByEmail(email) { // Ensure this function exists
+    async searchUserProfile(searchEmail) { // Ensure this function exists
         try {
             let initAdminProfileEntity = new profileEntity();
-            return await initAdminProfileEntity.searchProfileByEmail(email);
+            return await initAdminProfileEntity.searchUserProfile(searchEmail);
         } catch (err) {
             console.error("Error in userAdminSearchProfileController searchProfileByEmail:", err);
             throw new Error("Error searching profiles by email: " + err.message);
