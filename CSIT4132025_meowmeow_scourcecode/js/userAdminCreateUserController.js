7fe53cb74cb8d9a9a53f19a4531872639851793a
rachel2106 <rachel.seet64@gmail.com>
2025-04-29 08:50:38 +0800

    Update userAdminCreateUserController.js

--- CSIT4132025_meowmeow_scourcecode/js/userAdminCreateUserController.js
+++ CSIT4132025_meowmeow_scourcecode/js/userAdminCreateUserController.js
@@ -1,18 +1,10 @@
-import { profileEntity } from './profileEntity.js';
+import { userEntity } from "./userEntity.js";
 
-export class userAdminCreateProfileController {
-    async createProfile(profile) {
-        const profileEntityInstance = new profileEntity();
-
-        // Call createProfile in profileEntity to save it to Firestore
-        try {
-            await profileEntityInstance.createProfile(profile);
-            console.log("Profile successfully created");
-            return profile.name; // Or return the ID if needed
-        } catch (error) {
-            console.error("Error creating profile:", error);
-            throw error;
-        }
+export class adminCreateUserController {
+    async createUserAcc(newUserObj) {
+        const initAdminCUEntity = new userEntity(); //instantiates the userEntity class and assigns the variable
+        const message = await initAdminCUEntity.createUser(newUserObj); //calls the method then createUser is passed which contains details of the new user created
+        return message;
     }
 }
 
