530cc80fdcb69cd4fa47651078cf3ef0070dd03e
rachel2106 <rachel.seet64@gmail.com>
2025-05-13 00:08:20 +0800

    Update HMViewAllCSController.js

--- CSIT4132025_meowmeow_scourcecode/js/HMViewAllCSController.js
+++ CSIT4132025_meowmeow_scourcecode/js/HMViewAllCSController.js
@@ -1,16 +1,22 @@
-// HMViewAllCSController.js
 import { HomeownersEntity } from "./homeownersEntity.js";
 
-const entity = new HomeownersEntity();
-
 export class HMViewAllCSController {
-    static async getFilteredServices(filters) {
-        return await entity.fetchCleaningServices(filters);
-    }
+  constructor() {
+    this.entity = new HomeownersEntity();
+  }
 
-    static async getAllServices() {
-        return await entity.fetchAllCleaningServices();
-    }
+  // Fetch all services
+  async getAllServices() {
+    return await this.entity.fetchAllCleaningServices();
+  }
+
+  // Increment the view count for a given service
+  async incrementViewCount(categoryName, serviceId) {
+    return await this.entity.incrementViewCount(categoryName, serviceId);
+  }
+
+  // Increment the shortlist count for a given service
+  async incrementShortlistCount(categoryName, serviceId) {
+    return await this.entity.incrementShortlistCount(categoryName, serviceId);
+  }
 }
-
-
