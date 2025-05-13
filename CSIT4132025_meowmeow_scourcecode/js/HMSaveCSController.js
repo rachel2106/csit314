81d02366be5f8a3667c0b28fb7c508f91f47dab6
rachel2106 <rachel.seet64@gmail.com>
2025-05-14 04:07:59 +0800

    Update HMSaveCSController.js

--- CSIT4132025_meowmeow_scourcecode/js/HMSaveCSController.js
+++ CSIT4132025_meowmeow_scourcecode/js/HMSaveCSController.js
@@ -1,1 +1,12 @@
 
+import { HomeownersEntity } from './homeownersEntity.js';
+
+export class HMFavouritesController {
+  constructor() {
+    this.entity = new HomeownersEntity();
+  }
+
+  async getFavourites(userId) {
+    return await this.entity.getFavourites(userId);
+  }
+}
